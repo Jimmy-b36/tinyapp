25824a04d6abba679c86e38de58800fa4fc560ca
James Ball <jamie.j.ball@gmail.com>
2022-05-18 12:57:14 -0700

    add default for usersDb; add check for email/user

--- expressServer.js
+++ expressServer.js
@@ -6,38 +6,83 @@ const morgan = require('morgan');
 const port = 8080;
 app.set('view engine', 'ejs');
 
-//use morgan to see server actions
-app.use(morgan('dev'));
-//use body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+//------------------------------------------------
+//DATABASES
+//------------------------------------------------
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
 
-const usersDb = {};
+const usersDb = {
+  user1: {
+    username: 'user1',
+    password: 'password',
+    email: 'a@a.com',
+  },
+};
+
+// --------------------------------
+// FUNCTIONS
+// --------------------------------
+
+const randStringGen = function () {
+  let shortURL = '';
+  for (let i = 0; shortURL.length < 6; i++) {
+    let tmpStr = String.fromCharCode(Math.floor(Math.random() * 74 + 48));
+    tmpStr = tmpStr.replace(/[&\/\\#,+()$~%.;`^'":[\]*?<_>=@{}]/, 'q');
+    shortURL += tmpStr;
+  }
+  return shortURL;
+};
+
+const checkEmail = (email, username) => {
+  for (const user in usersDb) {
+    if (usersDb[user].email === email || usersDb[user].username === username) {
+      return false;
+    }
+    return true;
+  }
+};
+
+//----------------------------------------------------------
+// MIDDLEWARE
+//----------------------------------------------------------
+//use morgan to see server actions
+app.use(morgan('dev'));
+
+//use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// --------------------------------
+// ROUTES
+// --------------------------------
 
 //set the homepage response
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+// registration page
 app.get('/register', (req, res) => {
-  const templateVars = { username: req.cookies['username'] };
+  const templateVars = { username: req.cookies['userId'] };
   res.render('register', templateVars);
 });
 
 //page for creating new tinyUrls
 app.get('/urls/new', (req, res) => {
-  const templateVars = { username: req.cookies['username'] };
+  const templateVars = { username: req.cookies['userId'] };
   res.render('urls_new', templateVars);
 });
 
 //respond with the url database
 app.get('/urls', (req, res) => {
-  const templateVars = { username: req.cookies['username'], urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies['userId'],
+    urls: urlDatabase,
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -47,7 +92,7 @@ app.get('/urls/:shortURL', (req, res) => {
   console.log('params:', req.body);
 
   const templateVars = {
-    username: req.cookies[usersDb[username][username]],
+    username: req.cookies['userId'],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[shortURL],
   };
@@ -60,13 +105,17 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURL);
 });
 
+// ------------------------
+// POSTS
+// ------------------------
+
 //button to delete urls
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });
 
-//button to update urls
+// button to update urls
 app.post('/urls/:shortURL/update', (req, res) => {
   console.log('URLDB:', req.body);
   let shortURL = req.params.shortURL;
@@ -74,15 +123,33 @@ app.post('/urls/:shortURL/update', (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-//register form to post
-app.post('/register/new', (req, res) => {
-  usersDb[req.body.username] = {
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
+// registration form to create new user
+app.post('/register', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const username = req.body.username;
+
+  if (email === '' || password === '') {
+    return (
+      res.status(400).redirect('back'),
+      console.log('Please enter valid user || password')
+    );
+  }
+  if (!checkEmail(email, username)) {
+    return (
+      res.status(400).redirect('back'),
+      console.log('Email or username already taken')
+    );
+  }
+
+  usersDb[username] = {
+    username,
+    password,
+    email,
     awesomeness: req.body.awesome ? 'true' : 'false',
   };
-  res.cookie('userId', usersDb[req.body.username]);
+  console.log('Database', usersDb);
+  res.cookie('userId', usersDb[username].username);
   res.redirect('/urls');
 });
 
@@ -115,13 +182,3 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-function randStringGen() {
-  let shortURL = '';
-  for (let i = 0; shortURL.length < 6; i++) {
-    let tmpStr = String.fromCharCode(Math.floor(Math.random() * 74 + 48));
-    tmpStr = tmpStr.replace(/[&\/\\#,+()$~%.;`^'":[\]*?<_>=@{}]/, 'q');
-    shortURL += tmpStr;
-  }
-  return shortURL;
-}
